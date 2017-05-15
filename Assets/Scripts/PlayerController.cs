708cb53bc5e9374b6ffabeaab7ed25170a7e2dc2
Nick <nicholas.henegar@gmail.com>
2017-05-15 16:39:51 -0700

    initial upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private CharacterController pl;
+	// Use this for initialization
+	void Start () {
+        pl = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void FixedUpdate()
+    {
+        float moveHorizontal;
+    }
+}
